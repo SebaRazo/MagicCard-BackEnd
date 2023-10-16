de1aa9930e4b9a4a06a2cebd5162e16ead399521
seba-razo <sebarazo14@gmail.com>
2023-10-15 22:46:00 -0300

    Controllers restantes + Swagger + correcciones dependencias

--- src/main/java/com/back/MagicCard/controllers/AppUserController.java
+++ src/main/java/com/back/MagicCard/controllers/AppUserController.java
@@ -1,9 +1,70 @@
 package com.back.MagicCard.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.back.MagicCard.model.AppUser;
+import com.back.MagicCard.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/app-user")
 public class AppUserController {
+
+    private final AppUserService appUserService;
+
+    @Autowired
+    public AppUserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    // Crear un nuevo AppUser
+    @PostMapping
+    public ResponseEntity<AppUser> crearAppUser(@RequestBody AppUser appUser) {
+        AppUser nuevoAppUser = appUserService.registrarUser(appUser);
+        return new ResponseEntity<>(nuevoAppUser, HttpStatus.CREATED);
+    }
+
+    // Actualizar AppUser existente
+    @PutMapping("/{user_id}")
+    public ResponseEntity<AppUser> actualizarAppUser(@PathVariable("user_id") Long userId, @RequestBody AppUser appUser) {
+        Optional<AppUser> appUserExistente = appUserService.buscarUser(userId);
+        if (appUserExistente.isPresent()) {
+            appUser.setUser_id(userId);
+            AppUser appUserActualizado = appUserService.updateUser(appUser);
+            return new ResponseEntity<>(appUserActualizado, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Obtener un AppUser por su ID
+    @GetMapping("/{user_id}")
+    public ResponseEntity<AppUser> obtenerAppUser(@PathVariable("user_id") Long userId) {
+        Optional<AppUser> appUser = appUserService.buscarUser(userId);
+        return appUser.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // Obtener todos los AppUsers
+    @GetMapping
+    public ResponseEntity<List<AppUser>> obtenerTodosAppUsers() {
+        List<AppUser> appUsers = appUserService.buscarTodosAdmins();
+        return new ResponseEntity<>(appUsers, HttpStatus.OK);
+    }
+
+    // Eliminar un AppUser por su ID
+    @DeleteMapping("/{user_id}")
+    public ResponseEntity<Void> deleteAppUser(@PathVariable("user_id") Long userId) {
+        Optional<AppUser> appUserExistente = appUserService.buscarUser(userId);
+        if (appUserExistente.isPresent()) {
+            appUserService.deleteAdmin(userId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/back/MagicCard/controllers/SellerController.java
+++ src/main/java/com/back/MagicCard/controllers/SellerController.java
@@ -1,9 +1,70 @@
 package com.back.MagicCard.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.back.MagicCard.model.Seller;
+import com.back.MagicCard.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/seller")
+@RequestMapping("/sellers")
 public class SellerController {
+
+    private final SellerService sellerService;
+
+    @Autowired
+    public SellerController(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
+    // Crear un nuevo Seller
+    @PostMapping
+    public ResponseEntity<Seller> crearSeller(@RequestBody Seller seller) {
+        Seller nuevoSeller = sellerService.registrarSeller(seller);
+        return new ResponseEntity<>(nuevoSeller, HttpStatus.CREATED);
+    }
+
+    // Actualizar Seller existente
+    @PutMapping("/{seller_id}")
+    public ResponseEntity<Seller> actualizarSeller(@PathVariable("seller_id") Long sellerId, @RequestBody Seller seller) {
+        Optional<Seller> sellerExistente = sellerService.buscarSeller(sellerId);
+        if (sellerExistente.isPresent()) {
+            seller.setSeller_id(sellerId);
+            Seller sellerActualizado = sellerService.updateSeller(seller);
+            return new ResponseEntity<>(sellerActualizado, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Obtener un Seller por su ID
+    @GetMapping("/{seller_id}")
+    public ResponseEntity<Seller> obtenerSeller(@PathVariable("seller_id") Long sellerId) {
+        Optional<Seller> seller = sellerService.buscarSeller(sellerId);
+        return seller.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // Obtener todos los Sellers
+    @GetMapping
+    public ResponseEntity<List<Seller>> obtenerTodosSellers() {
+        List<Seller> sellers = sellerService.buscarTodosSellers();
+        return new ResponseEntity<>(sellers, HttpStatus.OK);
+    }
+
+    // Eliminar un Seller por su ID
+    @DeleteMapping("/{seller_id}")
+    public ResponseEntity<Void> deleteSeller(@PathVariable("seller_id") Long sellerId) {
+        Optional<Seller> sellerExistente = sellerService.buscarSeller(sellerId);
+        if (sellerExistente.isPresent()) {
+            sellerService.deleteSeller(sellerId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
